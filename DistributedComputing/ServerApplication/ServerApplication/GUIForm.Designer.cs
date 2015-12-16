87964d572d65b7014bc1ab98543ecb1a0aa39622
Robertorob <a_robert94@mail.ru>
2015-12-16 13:00:41 +0300

    relocation of controls

--- DistributedComputing/ServerApplication/ServerApplication/GUIForm.Designer.cs
+++ DistributedComputing/ServerApplication/ServerApplication/GUIForm.Designer.cs
@@ -38,9 +38,9 @@
             // 
             // richTextBox
             // 
-            this.richTextBox.Location = new System.Drawing.Point(24, 103);
+            this.richTextBox.Location = new System.Drawing.Point(12, 12);
             this.richTextBox.Name = "richTextBox";
-            this.richTextBox.Size = new System.Drawing.Size(350, 210);
+            this.richTextBox.Size = new System.Drawing.Size(268, 210);
             this.richTextBox.TabIndex = 1;
             this.richTextBox.Text = "";
             this.richTextBox.TextChanged += new System.EventHandler(this.richTextBox_TextChanged);
@@ -48,25 +48,24 @@
             // button_state
             // 
             this.button_state.Image = global::ServerApplication.Properties.Resources.start;
-            this.button_state.Location = new System.Drawing.Point(104, 15);
+            this.button_state.Location = new System.Drawing.Point(303, 12);
             this.button_state.Name = "button_state";
-            this.button_state.Size = new System.Drawing.Size(145, 61);
+            this.button_state.Size = new System.Drawing.Size(133, 61);
             this.button_state.TabIndex = 2;
             this.button_state.UseVisualStyleBackColor = true;
             this.button_state.Click += new System.EventHandler(this.button_state_Click);
             // 
             // textBox_task
             // 
-            this.textBox_task.Location = new System.Drawing.Point(381, 32);
-            this.textBox_task.Multiline = true;
+            this.textBox_task.Location = new System.Drawing.Point(107, 237);
             this.textBox_task.Name = "textBox_task";
-            this.textBox_task.Size = new System.Drawing.Size(112, 44);
+            this.textBox_task.Size = new System.Drawing.Size(214, 20);
             this.textBox_task.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(378, 9);
+            this.label1.Location = new System.Drawing.Point(15, 237);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(39, 13);
             this.label1.TabIndex = 4;
@@ -75,7 +74,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(568, 9);
+            this.label2.Location = new System.Drawing.Point(15, 281);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(86, 13);
             this.label2.TabIndex = 5;
@@ -83,17 +82,16 @@
             // 
             // textBox_range
             // 
-            this.textBox_range.Location = new System.Drawing.Point(571, 32);
-            this.textBox_range.Multiline = true;
+            this.textBox_range.Location = new System.Drawing.Point(107, 281);
             this.textBox_range.Name = "textBox_range";
-            this.textBox_range.Size = new System.Drawing.Size(114, 46);
+            this.textBox_range.Size = new System.Drawing.Size(214, 20);
             this.textBox_range.TabIndex = 6;
             // 
             // GUIForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(738, 338);
+            this.ClientSize = new System.Drawing.Size(448, 338);
             this.Controls.Add(this.textBox_range);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
