ab7f5594cc319279a72a59a04dd72f13dba87730
Dmitry Akishin <akishingoal@yandex.ru>
2015-12-17 19:23:39 +0300

    Client bug fix
    Исправлен клас с данными о загрузке ЦП

--- DistributedComputing/ClientApplication/ClientApplication/SystemHelper.cs
+++ DistributedComputing/ClientApplication/ClientApplication/SystemHelper.cs
@@ -12,12 +12,16 @@ namespace ClientApplication
     class SystemHelper
     {
         /* Класс, предназначенный для облегчения работы с системой и системными службами.
-         * Требуется подождать секунду, прежде чем использовать функции класса.
+         * Требуется подождать минимум секунду, прежде чем использовать функции класса.
          * Для этого в метод инициализации передается слушатель,
-         *  реализующий интерфейс InitListener с единственным методом OnInit(), вызываемым по завершении инициализации. */
+         *  реализующий интерфейс InitListener с единственным методом OnInit(), вызываемым по завершении инициализации. 
+         * Данные обновляются каждую секунду*/
 
         /* Счетчик загруженности процессора */
         private static PerformanceCounter totalCPU;
+
+        /* Загруженность процессора */
+        private static float CPUUsage;
 
         /* Слушатель завершения инициализации */
         private static InitListener initListener;
@@ -32,18 +36,31 @@ namespace ClientApplication
             timer.Elapsed += OnTimedEvent;
             timer.Enabled = true;
             timer.AutoReset = false;
+
+            // Таймер обновляния данных
+            System.Timers.Timer updateTimer = new System.Timers.Timer(1000);
+            updateTimer.Elapsed += UpdateData;
+            updateTimer.Enabled = true;
+            updateTimer.AutoReset = true;
         }
 
         /* Оповещение слушателя о завершении инициализации */
         private static void OnTimedEvent(Object source, ElapsedEventArgs e)
         {
+            CPUUsage = totalCPU.NextValue();
             initListener.OnInit();
+        }
+
+        /* Обновление данных */
+        private static void UpdateData(Object source, ElapsedEventArgs e)
+        {
+            CPUUsage = totalCPU.NextValue();
         }
 
         /* Загруженность процессора в процентах */
         public static float GetCPUUsage() 
         {
-            return totalCPU.NextValue();
+            return CPUUsage;
         }
 
         /* Интерфейс, реализуемый классом, желающим получить информацию
